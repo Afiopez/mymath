2c113eca5b3fcfe639dcabc30b4b4dc8977a6eeb
Afiopez <afcoin@yandex.ru>
2024-07-31 17:49:49 +0500

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -31,6 +31,16 @@ func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
 
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
 }
